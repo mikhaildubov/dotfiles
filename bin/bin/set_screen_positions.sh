55a9666d385af8bc4db6f85f625527078f739719
Pierre Dangauthier <pierre.dangauthier@smarkets.com>
2018-04-24 10:45:29 +0100

    3 horizontal screens

--- bin/bin/set_screen_positions.sh
+++ bin/bin/set_screen_positions.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output VIRTUAL1 --off --output DP3 --off --output DP2 --primary --mode 1920x1080 --pos 1080x440 --rotate normal --output DP1 --mode 1920x1080 --pos 0x0 --rotate left --output HDMI3 --off --output HDMI2 --off --output HDMI1 --mode 1920x1080 --pos 3000x440 --rotate normal
+xrandr --output VIRTUAL1 --off --output DP3 --off --output DP2 --primary --mode 1920x1080 --pos 1920x0 --rotate normal --output DP1 --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI3 --off --output HDMI2 --off --output HDMI1 --mode 1920x1080 --pos 3840x0 --rotate normal
